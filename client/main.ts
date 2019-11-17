c59985e2c15e3b183e316ea0f052f392a64f32df
Eti <nehu@Pixel>
2019-11-17 15:22:18 +0100

    Client-side execution flow

--- client/main.ts
+++ client/main.ts
@@ -1,15 +1,5 @@
-import { enableProdMode, PlatformRef } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
 import "./styles.scss";
 
-if (environment.production) {
-  enableProdMode();
-}
+import AngularStarter from "./angular-starter";
 
-let platform : PlatformRef = platformBrowserDynamic();
-
-platform.bootstrapModule(AppModule).catch(err => console.error(err));
+AngularStarter.get().start();

eb88720c913b59f3ef9981b4678203556013b84e
Eti <nehu@Pixel>
2019-11-19 22:09:16 +0100

    Leave kebab-case in the DOM

--- client/app-content/app-content.ts
+++ client/app-content/app-content.ts
@@ -2,12 +2,14 @@ import Vue from "vue";
 import Component from "vue-class-component";
 
 @Component({
-	template: "<h3>Welcome!</h3>"
+	template: "<h3>Welcome!</h3>" +
+		"<p>If you are seeing this, the pxserver-frontend library started up correctly, " +
+		"but no solution files have been loaded.</p>"
 })
 export default class AppContent extends Vue
 {
 	public static register() : void
 	{
-		Vue.component("app-content", AppContent);
+		Vue.component("AppContent", AppContent);
 	}
 }

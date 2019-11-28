5b7f3a2fa7c70a0e9eae6a2a518b3c2c4a062415
etienne-k <freydaklin@gmail.com>
2019-11-28 22:06:35 +0100

    Restructured

--- server/server-starter.ts
+++ server/server-starter.ts
@@ -1,86 +1,22 @@
-import * as http from "http";
-import * as redis from "redis";
+import * as core from "pxserver-core";
 
-import Server from "./server";
-
-export default class ServerStarter
+export default class ServerStarter extends core.AbstractServerStarter
 {
-	protected static SOCKET : string = process.env.SOCKET || "/opt/common/ipc.socket";
-
-	private server : Server;
-	private httpServer : http.Server;
-	private port : string;
-
-	private redisIn? : redis.RedisClient;
-	private redisInReady : boolean = false;
-	private redisOut? : redis.RedisClient;
-	private redisOutReady : boolean = false;
-
 	private constructor(root? : string)
 	{
-		this.server = Server.create(root);
-		this.port = process.env.PORT || "3000";
-
-		this.server.express.set("port", this.port);
-
-		this.httpServer = http.createServer(this.server.express);
-		this.httpServer.on("listening", () => this.onListening(this));
-		this.httpServer.on("error", (error) => this.onError(this, error));
-
-		//this.connectToBackend();
+		super(root);
 	}
 
-	public getServer() : Server
+	protected start() : ServerStarter
 	{
-		return this.server;
-	}
+		this.startHTTPServer();
 
-	private connectToBackend()
-	{
-		console.log("Connecting to the backend");
-
-		this.redisIn = redis.createClient(ServerStarter.SOCKET);
-
-		this.redisIn.on("ready", () => {
-			if (!this.redisIn) return;
-			this.redisInReady = true;
-			console.log("Subscribing");
-			this.redisIn.subscribe("backend-to-frontend:pxserver");
-		});
-		this.redisIn.on("message", (channel : string, message : string) => {
-			console.log("Channel <" + channel + ">: " + message);
-		});
-
-		this.redisOut = redis.createClient(ServerStarter.SOCKET);
-		this.redisOut.on("ready", () => {
-			if (!this.redisOut) return;
-			this.redisOutReady = true;
-			setTimeout(() => {
-				if (!this.redisOut) return;
-				console.log("Sending a message");
-				this.redisOut.publish("frontend-to-backend:pxserver", "Hello from Frontend!");
-			}, 3000)
-		});
-	}
-
-	private start() : void
-	{
-		this.httpServer.listen(this.port);
-	}
-
-	private onListening(starter : ServerStarter) : void
-	{
-		console.log("Listening on http://localhost:" + starter.port);
-	}
-
-	private onError(starter : ServerStarter, error : Error) : void
-	{
-		throw error;
+		return this;
 	}
 
 	public static start(root? : string) : ServerStarter
 	{
-		var starter : ServerStarter = new ServerStarter(root);
+		let starter : ServerStarter = new ServerStarter(root);
 
 		starter.start();
 
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,138 +1,11 @@
-import * as path from "path";
-import WebpackHelper from "./webpack.helper";
 import * as webpack from "webpack";
-
-const externals = require("webpack-node-externals");
+import { WebpackConfiguration } from "pxserver-core/webpack.config";
 
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
-export class WebpackConfiguration
+export class FrontendConfiguration extends WebpackConfiguration
 {
-	protected readonly libOutputDirectory : string = "lib";
-	protected readonly appOutputDirectory : string = "dist";
-
-	protected application : boolean = true;
-
-	public setLibrary() : WebpackConfiguration
-	{
-		this.application = false;
-		return this;
-	}
-
-	public isLibrary() : boolean
-	{
-		return !this.application;
-	}
-
-	public setApplication() : WebpackConfiguration
-	{
-		this.application = true;
-		return this;
-	}
-
-	public isApplication() : boolean
-	{
-		return this.application;
-	}
-
-	public getOutputDirectory() : string
-	{
-		return this.application ? this.appOutputDirectory : this.libOutputDirectory;
-	}
-
-	public getServerEntry() : webpack.Entry
-	{
-		let entry : webpack.Entry = {};
-
-		if (this.isApplication())
-			entry["Server"] = WebpackHelper.getPath("./server/main.ts");
-		else
-			entry["Server"] = WebpackHelper.getPath("./server/module.ts");
-
-		return entry;
-	}
-
-	public getClientEntry() : webpack.Entry
-	{
-		let entry : webpack.Entry = {};
-
-		entry["polyfills"] = WebpackHelper.getPath("./client/polyfills.ts");
-		entry["app"] = WebpackHelper.getPath("./client/main.ts");
-
-		return entry;
-	}
-
-	protected getServerOutput() : webpack.Output
-	{
-		let output : webpack.Output = {};
-
-		output.path = path.resolve(".", this.getOutputDirectory());
-
-		if (this.isApplication())
-		{
-			output.filename = "main.js";
-			output.library = "main";
-			output.libraryTarget = "umd";
-		}
-		else
-		{
-			output.filename = "module.js";
-			output.library = "module";
-			output.libraryTarget = "umd";
-		}
-
-		return output;
-	}
-
-	protected getClientOutput() : webpack.Output
-	{
-		let output : webpack.Output = {};
-
-		output.path = path.resolve(".", this.getOutputDirectory(), "public");
-		output.filename = "[name].bundle.js";
-		output.chunkFilename = "[name].bundle.js";
-
-		return output;
-	}
-
-	public getServerConfig() : webpack.Configuration
-	{
-		return {
-			target: "node",
-			mode: "development",
-			devtool: "inline-source-map",
-			entry: this.getServerEntry(),
-			module: {
-				rules: [
-					{
-						test: /\.tsx?$/,
-						loaders: [
-							{
-								loader: "ts-loader",
-								options: { configFile: "tsconfig.server.json" }
-							}
-						],
-						exclude: /node_modules/
-					}
-				]
-			},
-			resolve: {
-				extensions: [".tsx", ".ts", ".js"],
-				alias: {
-					"hiredis": path.resolve(__dirname, "server", "helpers", "hiredis.js")
-				}
-			},
-			output: this.getServerOutput(),
-			node: {
-				// Do not let Webpack change these globals
-				__dirname: false,
-				__filename: false,
-			},
-			externals: [externals()]
-		};
-	}
-
 	public getClientConfig() : webpack.Configuration
 	{
 		return {
@@ -181,7 +54,7 @@ export class WebpackConfiguration
 			output: this.getClientOutput(),
 			plugins: [
 				new HTMLWebpackPlugin({
-					template: WebpackHelper.getPath("./client/index.html")
+					template: this.getPath("./client/index.html")
 				}),
 				new VueLoaderPlugin()
 			],
@@ -199,4 +72,4 @@ export class WebpackConfiguration
 	}
 }
 
-export default new WebpackConfiguration().setLibrary().get();
+export default new FrontendConfiguration().setHybrid().get();
